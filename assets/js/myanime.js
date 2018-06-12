edf8242a3c206d662c8f42e621f36ad9530f7403
= <=>
2018-06-12 02:18:13 +0200

    layout

--- assets/js/myanime.js
+++ assets/js/myanime.js
@@ -12,11 +12,7 @@ var rightLeft = anime({
    opacity: [
 	{ value: [0, 1], easing: 'easeOutBack' }
   ],
-  scale: {
-    value: 2,
-    delay: 800,
-  },
-
+  borderRadius: '5px',
   /*rotate: function() { return anime.random(-360, 360); },
   duration: function() { return anime.random(1200, 1800); },
   duration: function() { return anime.random(800, 1600); },
@@ -28,5 +24,32 @@ var rightLeft = anime({
 document.querySelector('.rightLeft .reverse').onclick = function() {
   rightLeft.play();
   rightLeft.reverse();
+};
+
+var rightTop = anime({
+  targets: '.rightTop .el',
+  /*translateX: function(el){
+  	return el.getAttribute('data-x');
+  },
+    translateY: function(el){
+  	return el.getAttribute('data-y');
+  },*/
+  left: [{ value: '5%', duration: 3000 }],
+   opacity: [
+	{ value: [0, 1], easing: 'easeOutBack' }
+  ],
+  borderRadius: '5px',
+  /*rotate: function() { return anime.random(-360, 360); },
+  duration: function() { return anime.random(1200, 1800); },
+  duration: function() { return anime.random(800, 1600); },
+  delay: function() { return anime.random(0, 1000); },
+  direction: 'alternate',*/
+  autoplay: false
+});
+
+document.querySelector('.rightTop .reverse').onclick = function() {
+  rightTop.play();
+  rightTop.reverse();
 }
+
 });
